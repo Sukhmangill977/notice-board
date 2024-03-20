862b925c085fd6ed0de5fc83b5781cebf835d1c3
gundeeps247 <gundeepsinghm@gmail.com>
2024-03-21 01:25:19 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const noticeRoutes = require('./routes/noticeRoutes');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors())
 
 // Middleware 
 app.use(bodyParser.json());
