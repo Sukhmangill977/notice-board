3c6741d91aef0178394f048dac92ecbf28351e3b
Sukhmangill977 <sukhmangill977@gmail.com>
2024-04-03 15:08:25 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import NoticeBoard from './components/NoticeBoard';
-import AddNotice from './components/AddNotice';
-import './App.css'; // Import CSS file for styling
+import React, { useState } from "react";
+import NoticeBoard from "./components/NoticeBoard";
+import AddNotice from "./components/AddNotice";
+import "./App.css"; // Import CSS file for styling
+import Navbar from "./components/Navbar.jsx";
+import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [showLoginForm, setShowLoginForm] = useState(false); // Initially hidden
 
   const handleLogin = (username, password) => {
-    if (username === 'gundeep' && password === 'qwerty') {
+    if (username === "gundeep" && password === "qwerty") {
       setLoggedIn(true);
       setShowLoginForm(false); // Hide form on successful login
     } else {
-      alert('Invalid username or password');
+      alert("Invalid username or password");
     }
   };
 
@@ -21,36 +23,45 @@ function App() {
   };
 
   return (
+    <Router>
     <div className="App">
-      <NoticeBoard loggedIn={loggedIn} />
-      <div className="login-button">
-        {!loggedIn && ( // Show login button if not logged in
-          <button className="login-button-text" onClick={toggleLoginForm}>Login</button>
-        )}
-      </div>
-      {showLoginForm && ( // Show login form if button pressed
-        <div className="login-container">
-          <h2>Login</h2>
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            const username = e.target.username.value;
-            const password = e.target.password.value;
-            handleLogin(username, password);
-          }}>
-            <label>
-              Username:
-              <input type="text" name="username" />
-            </label>
-            <label>
-              Password:
-              <input type="password" name="password" />
-            </label>
-            <button type="submit" className="login-button-text">Login</button>
-          </form>
+        <Navbar />
+        <NoticeBoard loggedIn={loggedIn} />
+        <div className="login-button">
+          {!loggedIn && ( // Show login button if not logged in
+            <button className="login-button-text" onClick={toggleLoginForm}>
+              Login
+            </button>
+          )}
         </div>
-      )}
-      {loggedIn && <AddNotice />} {/* Render AddNotice only if logged in */}
-    </div>
+        {showLoginForm && ( // Show login form if button pressed
+          <div className="login-container">
+            <h2>Login</h2>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                const username = e.target.username.value;
+                const password = e.target.password.value;
+                handleLogin(username, password);
+              }}
+            >
+              <label>
+                Username:
+                <input type="text" name="username" />
+              </label>
+              <label>
+                Password:
+                <input type="password" name="password" />
+              </label>
+              <button type="submit" className="login-button-text">
+                Login
+              </button>
+            </form>
+          </div>
+        )}
+        {loggedIn && <AddNotice />} {/* Render AddNotice only if logged in */}
+      </div>
+      </Router>
   );
 }
 
